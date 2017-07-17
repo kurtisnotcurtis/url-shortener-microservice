3216a4a0f3bf4f5750cb9120a3a26335e916ad5e
Kurt Galiatsatos <soad1789@gmail.com>
2017-07-17 21:00:04 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const path = require("path");
+
 const pug = require("pug");
-const homepage = pug.compileFile( path.join(__dirname, "public", "index.pug") );
+const homepage = pug.compileFile( path.join(__dirname, "views", "index.pug") );
+
 const port = process.env.PORT || 3000;
 var app = express();
 
+app.set("view engine", "pug");
+
 app.get("/", function (req, res) { // Serve homepage (static view)
-  var fileName = path.join(__dirname, "views", "index.html");
-  res.sendFile(fileName, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    } else {
-      console.log("Serving view:", fileName);
-    }
-  });
+  res.render(path.join(__dirname, "views", "index"));
 });
 
 app.get("/:url", function (req, res) { // Handle passed url param
