b1952976ef4a976bcf2da9a08b4a071d8f113801
Kurt Galiatsatos <soad1789@gmail.com>
2017-07-18 17:34:00 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 
+const mongodb = require('mongodb'); // Let's import the mongodb native drivers
+const MongoClient = mongodb.MongoClient; // We need to work with "MongoClient" interface in order to connect to a mongodb server
+const url = process.env.MONGOLAB_URI; // Connection URL. This is where your mongodb server is running.
+
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -23,4 +27,18 @@ app.get("/:url", function (req, res) { // Handle passed url param
   res.json();
 });
 
-app.listen(port);
+app.listen(port);  
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
